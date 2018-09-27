1ff72fbb352c996a58c9dd07d0137076a24a3628
Krists <kpudzens@gmail.com>
2018-09-27 10:59:13 +0300

    u

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -21,6 +21,7 @@ Java_com_nativetest_nativetestapp_MainActivity_createFMODJNI(
 
     FMOD_RESULT       result;
     unsigned int      version;
+    int      numDrivers;
 
     result = FMOD::System_Create(&fmod_system);
     if (result != FMOD_OK){
@@ -30,9 +31,26 @@ Java_com_nativetest_nativetestapp_MainActivity_createFMODJNI(
     result = fmod_system->getVersion(&version);
     addConsoleLine(" FMOD version: "+std::to_string(version));
 
+    result = fmod_system->getNumDrivers(&numDrivers);
+    if (result != FMOD_OK){
+        addConsoleLine(" FMOD:getNumDrivers failed");
+        return 0;
+    }
+    addConsoleLine(" FMOD num drivers: "+std::to_string(numDrivers));
+
+    for(int i=0; i<numDrivers; i++) {
+        int szLength = 1024;
+        char szName[szLength] = {0};
+        FMOD_GUID sFmodGuid;
+        result = fmod_system->getDriverInfo(i, szName, szLength, &sFmodGuid, 0, 0, 0);
+        if (result == FMOD_OK){
+            addConsoleLine(" FMOD driver: "+std::to_string(i)+"  name: "+std::string(szName));
+        }
+    }
 
 
-    return 0;
+
+    return 1;
 }
 
 
