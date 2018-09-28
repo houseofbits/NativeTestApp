6856e750ae996aa0ad1a4b13d488256a6c6f62a8
Krists <kpudzens@gmail.com>
2018-09-28 15:15:30 +0300

    d

--- app/src/main/java/com/nativetest/nativetestapp/MainActivity.java
+++ app/src/main/java/com/nativetest/nativetestapp/MainActivity.java
@@ -1,34 +1,31 @@
 package com.nativetest.nativetestapp;
 
 import android.os.Bundle;
+import android.support.v4.view.InputDeviceCompat;
 import android.util.Log;
+import android.view.InputDevice;
+import android.view.MotionEvent;
 import android.widget.CompoundButton;
 import android.widget.MultiAutoCompleteTextView;
 import android.widget.Switch;
-import android.widget.TextView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
 import android.view.MenuItem;
 
-import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 import android.hardware.usb.UsbDevice;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import com.google.android.things.pio.PeripheralManager;
+import com.google.android.things.pio.UartDevice;
+import com.google.android.things.pio.UartDeviceCallback;
 
-import android.view.InputDevice;
-import android.view.InputDevice.MotionRange;
-import android.view.MotionEvent;
+import java.io.IOException;
+import java.util.List;
 
 //<permissions>
 //        <feature name="android.hardware.usb.host"/>
 
-public class MainActivity extends AppCompatActivity implements View.OnGenericMotionListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "NativetestLog";
 
@@ -36,16 +33,10 @@ public class MainActivity extends AppCompatActivity implements View.OnGenericMot
 //    private PlayerThread playerThread;
     private String debugData = "";
 
+    private UartDevice mDevice = null;
+
     // Used to load the 'native-lib' library on application startup.
     static {
-        /*
-         * To simplify our examples we try to load all possible FMOD
-         * libraries, the Android.mk will copy in the correct ones
-         * for each example. For real products you would just load
-         * 'fmod' and if you use the FMOD Studio tool you would also
-         * load 'fmodstudio'.
-         */
-
         // Try debug libraries...
         try {
             System.loadLibrary("fmodD");
@@ -118,7 +109,23 @@ public class MainActivity extends AppCompatActivity implements View.OnGenericMot
             this.addDebugString(" Serial number: "+serialNumber);
         }
 
-        initController();
+        checkController();
+
+        //UART Devices
+        try{
+            PeripheralManager manager = PeripheralManager.getInstance();
+        }catch(Exception i){
+            this.addDebugString(i.getMessage());
+        }
+        //this.addDebugString(manager.toString());
+//        List<String> deviceList = manager.getUartDeviceList();
+//        if(deviceList.isEmpty()){
+//            this.addDebugString("No UART port available on this device.");
+//        }else{
+//            for (int i=0; i<deviceList.size(); i++){
+//                this.addDebugString("UART Device: " + deviceList.get(i));
+//            }
+//        }
 
         org.fmod.FMOD.init(this);
 
@@ -143,54 +150,125 @@ public class MainActivity extends AppCompatActivity implements View.OnGenericMot
         //fixedThreadPool = Executors.newFixedThreadPool(1);
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onDestroy() {
+        super.onDestroy();
 
-        //noinspection SimplifiableIfStatement
-//        if (id == R.id.switch_console) {
-//            MultiAutoCompleteTextView tv = findViewById(R.id.multiAutoCompleteTextView);
-//            if(item.isChecked()){
-//                tv.setVisibility(View.VISIBLE);
-//            }else{
-//                tv.setVisibility(View.INVISIBLE);
-//            }
-//
-//            return true;
-//        }
+        if (mDevice != null) {
+            try {
+                mDevice.close();
+                mDevice = null;
+            } catch (IOException e) {
+                this.addDebugString("Unable to close UART device: "+ e);
+            }
+        }
+    }
+/*
+    public void openUARTDevice(String devicename) throws IOException {
+        // Attempt to access the UART device
+        try{
+            PeripheralManager manager = PeripheralManager.getInstance();
+            mDevice = manager.openUartDevice(devicename);
+        }catch (IOException e){
+            this.addDebugString("Unable to access UART device: "+e);
+        }
+        if(mDevice != null) {
+            mDevice.setBaudrate(115200);
+            mDevice.setDataSize(8);
+            mDevice.setParity(UartDevice.PARITY_NONE);
+            mDevice.setStopBits(1);
+            mDevice.setHardwareFlowControl(UartDevice.HW_FLOW_CONTROL_NONE);
+        }
+    }
+    public void writeUARTData(byte data[]) throws IOException {
+        if(mDevice != null){
+            mDevice.write(data, data.length);
+        }
+    }
+    */
 
-        return super.onOptionsItemSelected(item);
+    public void checkController(){
+        for(int deviceId: InputDevice.getDeviceIds()) {
+            InputDevice device = InputDevice.getDevice(deviceId);
+            String controllerName = device.getName();
+            this.addDebugString("Controller: "+controllerName+" is constroller "+isController(device));
+        }
     }
 
-    public void initController(){
-
-        for(int deviceId: InputDevice.getDeviceIds()) {
-
-            InputDevice device = InputDevice.getDevice(deviceId);
-
-            String controllerName = device.getName();
-
-            this.addDebugString("Controller: "+controllerName);
-
+    @Override
+    public boolean dispatchGenericMotionEvent(final MotionEvent event) {
+        if(!isController(event.getDevice())){
+        //if((event.getSource()&InputDeviceCompat.SOURCE_JOYSTICK)!=InputDeviceCompat.SOURCE_JOYSTICK ){
+            return super.dispatchGenericMotionEvent(event);
         }
 
-        //this.addGenericMotionListener(this);
-    }
-    @Override
-    public boolean onGenericMotion (View view, MotionEvent motionEvent) {
+        int historySize = event.getHistorySize();
+        for (int i = 0; i < historySize; i++) {
+            // Process the event at historical position i
+            this.addDebugString("JOYSTICKMOVE "+event.getHistoricalAxisValue(MotionEvent.AXIS_Y,i)+"  "+event.getHistoricalAxisValue(MotionEvent.AXIS_Z,i));
+            processJoystickInput(event,  i);
+        }
+        // Process current position
+        this.addDebugString("JOYSTICKMOVE "+event.getAxisValue(MotionEvent.AXIS_Y)+" "+event.getAxisValue(MotionEvent.AXIS_Z));
 
         return true;
     }
+    private void processJoystickInput(MotionEvent event, int historyPos) {
+
+        InputDevice mInputDevice = event.getDevice();
+
+        // Calculate the horizontal distance to move by
+        // using the input value from one of these physical controls:
+        // the left control stick, hat axis, or the right control stick.
+        float x = getCenteredAxis(event, mInputDevice, MotionEvent.AXIS_X, historyPos);
+        if (x == 0) {
+            x = getCenteredAxis(event, mInputDevice, MotionEvent.AXIS_HAT_X, historyPos);
+        }
+        if (x == 0) {
+            x = getCenteredAxis(event, mInputDevice, MotionEvent.AXIS_Z, historyPos);
+        }
+
+        // Calculate the vertical distance to move by
+        // using the input value from one of these physical controls:
+        // the left control stick, hat switch, or the right control stick.
+        float y = getCenteredAxis(event, mInputDevice, MotionEvent.AXIS_Y, historyPos);
+        if (y == 0) {
+            y = getCenteredAxis(event, mInputDevice, MotionEvent.AXIS_HAT_Y, historyPos);
+        }
+        if (y == 0) {
+            y = getCenteredAxis(event, mInputDevice, MotionEvent.AXIS_RZ, historyPos);
+        }
+
+        // Update the ship object based on the new x and y values
+    }
+    private static float getCenteredAxis(MotionEvent event, InputDevice device, int axis, int historyPos) {
+        final InputDevice.MotionRange range =
+                device.getMotionRange(axis, event.getSource());
+
+        // A joystick at rest does not always report an absolute position of
+        // (0,0). Use the getFlat() method to determine the range of values
+        // bounding the joystick axis center.
+        if (range != null) {
+            final float flat = range.getFlat();
+            final float value =
+                    historyPos < 0 ? event.getAxisValue(axis):
+                            event.getHistoricalAxisValue(axis, historyPos);
+
+            // Ignore axis values that are within the 'flat' region of the
+            // joystick axis center.
+            if (Math.abs(value) > flat) {
+                return value;
+            }
+        }
+        return 0;
+    }
+
+    private boolean isController(InputDevice device) {
+        return ((device.getSources() & InputDevice.SOURCE_CLASS_JOYSTICK) == InputDevice.SOURCE_CLASS_JOYSTICK)
+                && (((device.getSources() & InputDevice.SOURCE_GAMEPAD) == InputDevice.SOURCE_GAMEPAD)
+                || (device.getKeyboardType() != InputDevice.KEYBOARD_TYPE_ALPHABETIC));
+    }
+
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
